bb841e5cec729e8b4ca2f5b06e6706fbbba87974
aca <dpscamp12@gmail.com>
2012-10-15 15:33:38 +0200

    UI finetuning

---
+++ Zmapp/Zuehlke.Zmapp.Wpf.Tests/EmployeeListViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zuehlke.Zmapp.Services.Contracts.Employee;
+
+namespace Zuehlke.Zmapp.Wpf.Tests
+{
+    [TestClass]
+    public class EmployeeListViewModelTests
+    {
+        [TestMethod]
+        public void Skills_AllEnumValuesReturned()
+        {
+            var viewModel = new EmployeeListViewModel(new EmployeeEvaluationServiceMock());
+            Assert.AreEqual(6, viewModel.Skills.Count());
+            Assert.IsTrue(viewModel.Skills.Contains(Skill.WCF));
+        }
+    }
+}
--- Zmapp/Zuehlke.Zmapp.Wpf/EmployeeListViewModel.cs
+++ Zmapp/Zuehlke.Zmapp.Wpf/EmployeeListViewModel.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zuehlke.Zmapp.Services.Contracts.Employee;
 
 namespace Zuehlke.Zmapp.Wpf
 {
     public class EmployeeListViewModel
     {
+        private readonly IEmployeeEvaluationService service = new EmployeeEvaluationServiceMock();
 
+        public EmployeeListViewModel()
+        {
+        }
+
+        public EmployeeListViewModel(IEmployeeEvaluationService injectedService)
+        {
+            this.service = injectedService;
+        }
+
+        public IEnumerable<CustomerInfo> Customers
+        {
+            get
+            {
+                return service.GetCustomers();
+            }
+        }
+
+        public IEnumerable<Skill> Skills
+        {
+            get { return typeof(Skill).GetEnumValues().Cast<Skill>(); }
+        }
     }
 }
