4b23e5b84c0302b67a33316a549fd0394077617c
aca <dpscamp12@gmail.com>
2012-10-15 10:17:26 +0200

    Fix for service mock

--- Zmapp/Zuehlke.Zmapp.Services.Contracts/Employee/IEmployeeEvaluationService.cs
+++ Zmapp/Zuehlke.Zmapp.Services.Contracts/Employee/IEmployeeEvaluationService.cs
@@ -11,26 +11,4 @@ namespace Zuehlke.Zmapp.Services.Contracts.Employee
         [OperationContract]
         EmployeeSearchResult[] FindPotentialEmployeesForCustomer(EmployeeQuery query);
     }
-
-    public class EmployeeEvaluationServiceMock : IEmployeeEvaluationService
-    {
-        public CustomerInfo[] GetCustomers()
-        {
-            return new[] { new CustomerInfo() { Id = 1, Name = "Customer 1" } };
-        }
-
-        public EmployeeSearchResult[] FindPotentialEmployeesForCustomer(EmployeeQuery query)
-        {
-            var e = new EmployeeSearchResult()
-                        {
-                            Distance = 10.3f,
-                            EmployeeName = "Hans Wurst",
-                            Id = 1,
-                            Level = CareerLevel.SoftwareEngineer,
-                            Skills = new[] { Skill.WCF, Skill.AspDotNet, }
-                        };
-
-            return new[] { e };
-        }
-    }
 }
