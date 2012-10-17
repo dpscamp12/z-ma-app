46f3b9f717d49a05e8f31c8366a9af16ac34a529
aca <dpscamp12@gmail.com>
2012-10-17 16:37:11 +0200

    New View for editing customers

---
+++ Zmapp/Zuehlke.Zmapp.Wpf/CustomersViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System.Collections.Generic;
+using System.Linq;
+using Zuehlke.Zmapp.Services.Contracts.Customers;
+using Zuehlke.Zmapp.Services.Contracts.Employee;
+using Zuehlke.Zmapp.Wpf.Tools;
+
+namespace Zuehlke.Zmapp.Wpf
+{
+	public class CustomersViewModel : NotificationObject
+	{
+		private readonly ICustomerService service;
+		private readonly List<CustomerInfo> customers = new List<CustomerInfo>();
+		private CustomerInfo selectedCustomer;
+
+
+		public CustomersViewModel()
+		{
+			this.Init();
+		}
+
+		public CustomersViewModel(ICustomerService injectedService)
+			: this()
+		{
+			this.service = injectedService;
+		}
+
+		public void Init()
+		{
+			this.customers.Clear();
+			CustomerInfo[] customerInfos = this.service.GetCustomers();
+			this.customers.AddRange(customerInfos);
+
+			this.selectedCustomer = this.customers.FirstOrDefault();
+		}
+
+		public IEnumerable<CustomerInfo> Customers
+		{
+			get { return this.customers; }
+		}
+
+		public MultiSelectCollectionView<CareerLevel> CareerLevels { get; private set; }
+
+
+		public CustomerInfo SelectedCustomer
+		{
+			get
+			{
+				return this.selectedCustomer;
+			}
+			set
+			{
+				this.selectedCustomer = value;
+				this.RaisePropertyChanged(() => this.SelectedCustomer);
+			}
+		}
+	}
+}
