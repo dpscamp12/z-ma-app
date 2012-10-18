bea557e84a8e4da5c880953c602995d9e36897a8
aca <dpscamp12@gmail.com>
2012-10-18 15:17:38 +0200

    - Enums for Skills and CareerLevels enhanced - Further UI refinement

--- Zmapp/Zuehlke.Zmapp.Services.Contracts/Employees/CareerLevel.cs
+++ Zmapp/Zuehlke.Zmapp.Services.Contracts/Employees/CareerLevel.cs
@@ -2,12 +2,20 @@ using System.Runtime.Serialization;
 
 namespace Zuehlke.Zmapp.Services.Contracts.Employees
 {
-    [DataContract]
-    public enum CareerLevel
-    {
-        [EnumMember]
-        JuniorSoftwareEngineer = 0,
-        [EnumMember]
-        SoftwareEngineer = 1
-    }
+	[DataContract]
+	public enum CareerLevel
+	{
+		[EnumMember]
+		JuniorSoftwareEngineer = 0,
+		[EnumMember]
+		SoftwareEngineer = 1,
+		[EnumMember]
+		SeniorSoftwareEngineer = 2,
+		[EnumMember]
+		LeadSoftwareArchitect = 3,
+		[EnumMember]
+		PrincipalConsultant = 4,
+		[EnumMember]
+		ProjectManager = 5
+	}
 }
--- Zmapp/Zuehlke.Zmapp.Services.Contracts/Employees/Skill.cs
+++ Zmapp/Zuehlke.Zmapp.Services.Contracts/Employees/Skill.cs
@@ -2,22 +2,22 @@ using System.Runtime.Serialization;
 
 namespace Zuehlke.Zmapp.Services.Contracts.Employees
 {
-    [DataContract]
-    public enum Skill
-    {
-        [EnumMember]
-        None = 0,
-        [EnumMember]
-        CSharp = 1,
-        [EnumMember]
-        SqlServer = 2,
-        [EnumMember]
-        AspDotNet = 3,
-        [EnumMember]
-        CloudComputing = 4,
-        [EnumMember]
-        WPF = 5,
-        [EnumMember]
-        WCF = 6
-    }
+	[DataContract]
+	public enum Skill
+	{
+		[EnumMember]
+		Windows7 = 0,
+		[EnumMember]
+		CSharp = 1,
+		[EnumMember]
+		SqlServer = 2,
+		[EnumMember]
+		AspDotNet = 3,
+		[EnumMember]
+		CloudComputing = 4,
+		[EnumMember]
+		WPF = 5,
+		[EnumMember]
+		WCF = 6
+	}
 }
