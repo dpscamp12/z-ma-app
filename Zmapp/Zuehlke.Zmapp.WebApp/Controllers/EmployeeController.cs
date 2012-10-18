ed072917f49ab964d28fd6233b2e961597cc5e94
aca <dpscamp12@gmail.com>
2012-10-18 10:34:46 +0200

    Location of service adjusted

--- Zmapp/Zuehlke.Zmapp.WebApp/Controllers/EmployeeController.cs
+++ Zmapp/Zuehlke.Zmapp.WebApp/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using Zuehlke.Zmapp.Services.Contracts.Customers;
 using Zuehlke.Zmapp.Services.Contracts.Employee;
 using Zuehlke.Zmapp.Services.Customers;
+using Zuehlke.Zmapp.Services.Employees;
 using Zuehlke.Zmapp.WebApp.Models;
 
 namespace Zuehlke.Zmapp.WebApp.Controllers
