c1be5f80f758fc77ef1eb0b7525f28f5236e7a3d
aca <dpscamp12@gmail.com>
2012-10-18 14:22:46 +0200

    Renaming

---
+++ Zmapp/Zuehlke.Zmapp.Wpf/EmployeeReservation.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Zuehlke.Zmapp.Wpf
+{
+	/// <summary>
+	/// Interaction logic for EmployeeReservation.xaml
+	/// </summary>
+	public partial class EmployeeReservation
+	{
+		private readonly EmployeeListViewModel viewModel = new EmployeeListViewModel();
+
+		public EmployeeReservation()
+		{
+			this.InitializeComponent();
+
+			this.viewModel.Init();
+			this.DataContext = this.viewModel;
+		}
+	}
+}
